99685cddd96d85ef439259b83357a5b2c56488df
zblocksproduct <151188302+zblocksproduct@users.noreply.github.com>
2023-11-29 11:28:21 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Chofy />} />
-          <Route path="chofyLanding" element={<ChofyLanding />} />
+          <Route path="experience" element={<ChofyLanding />} />
         </Routes>
       </Router>
     </div>
